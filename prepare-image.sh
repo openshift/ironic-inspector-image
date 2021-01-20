21ab59e1a40e5fef56fce48391f71b821300cd6c
Riccardo Pittau <elfosardo@gmail.com>
2021-01-20 16:54:51 +0100

    Introduce separate ARG for extra packages list
    Convert the current PKGS_LIST to ENV as we should not really touch it.
    Introduce the EXTRA_PKGS_LIST ARG to allow specifying a file
    containing a list of extra packages to install.
    The goal is to allow installing packages from different sources
    to test them.

--- prepare-image.sh
+++ prepare-image.sh
@@ -1,9 +1,14 @@
  #!/usr/bin/bash
 
-set -ex
+set -euxo pipefail
 
 dnf upgrade -y
-dnf install -y $(cat /tmp/main-packages-list.txt)
+dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${PKGS_LIST})
+if [[ ! -z ${EXTRA_PKGS_LIST:-} ]]; then
+    if [[ -s /tmp/${EXTRA_PKGS_LIST} ]]; then
+        dnf --setopt=install_weak_deps=False install -y $(cat /tmp/${EXTRA_PKGS_LIST})
+    fi
+fi
 mkdir -p /var/lib/ironic-inspector
 sqlite3 /var/lib/ironic-inspector/ironic-inspector.db "pragma journal_mode=wal"
 dnf remove -y sqlite
